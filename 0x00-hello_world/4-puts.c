5c2373a48aba6f7b16511e26943511c0fe6159e3
phareedahadamu <phareedahadamu@gmail.com>
2023-02-16 17:12:38 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+/**
+ * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: 0 (Success)
+ */ 
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
-
-
