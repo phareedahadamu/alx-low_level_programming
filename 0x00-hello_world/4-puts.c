d8b3cea36c54affdc31b783b0f5d3c0f564d305d
phareedahadamu <phareedahadamu@gmail.com>
2023-02-16 17:32:16 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * main- A program that prints a line using puts function
  * Return: 0 (Success)
- */ 
-int main (void)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
