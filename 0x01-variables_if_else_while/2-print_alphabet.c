4b149e3ece7537e7b12ec3607e6e93788aee10f4
phareedahadamu <phareedahadamu@gmail.com>
2023-02-19 17:08:38 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * main- print in lowercase
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		x = tolower(x);
+		putchar(x);
+	}
+	return (0);
+}
