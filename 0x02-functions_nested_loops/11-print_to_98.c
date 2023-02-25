62b075201c3b5ba6cb469ef48237ae7e72c7cf3f
phareedahadamu <phareedahadamu@gmail.com>
2023-02-25 06:29:17 -0800

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,27 +6,27 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
-
-	if (n <= 98)
+	if (n > 98)
 	{
-		for (i = n; i <= 98; i++)
+		while (n >= 98)
 		{
-			if (i != 98)
-				printf("%d,", i);
-			else
-				printf("%d\n", i);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
 		}
 	}
 
-	else if (n >= 98)
+	else
 	{
-		for (j = n; j <= 98; j--)
+		while (n <= 98)
 		{
-			if (j != 98)
-				printf("%d,", j);
-			else
-				printf("%d\n", j);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
 		}
 	}
+	putchar('\n');
 }
+
