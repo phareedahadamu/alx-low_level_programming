56debd7bc4df03c81d2765d9cd2ed1151ae9c226
phareedahadamu <phareedahadamu@gmail.com>
2023-02-25 09:51:45 -0800

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,6 +9,7 @@ int main(void)
 	unsigned long n1 = 1;
 	unsigned long n2 = 2;
 	unsigned long n3;
+	unsigned int n4;
 	int n = 50;
 	int i;
 
@@ -19,10 +20,14 @@ int main(void)
 	n3 = n1 + n2;
 	for (i = 3; i <= n; i++)
 	{
-		printf("%ld, ", n3);
+		printf("%ld", n3);
 		n1 = n2;
 		n2 = n3;
 		n3 = n1 + n2;
+		if (i < n)
+		{
+			printf(", ");
+		}
 	}
 	putchar('\n');
 	return (0);
