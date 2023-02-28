0ac3c5f01677b506676b3fa7d17891ad735b1599
phareedahadamu <phareedahadamu@gmail.com>
2023-02-28 04:05:33 -0800

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi -converts a string to an integer
+ * @s: the string
+ * Return: 0 returned if no numbers in the string
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, n = 0, sign = 1;
+
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	sign *= -1;
+	return (n * sign);
+}
