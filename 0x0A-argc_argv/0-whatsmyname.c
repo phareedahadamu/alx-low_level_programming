61aba22bdcaa95f497ae3b9efaff17b3ace5707f
Farida <phareedahadamu@gmail.com>
2023-03-13 15:35:12 +0100

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -print the name of the file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+		printf("%s\n", argv[0]);
+		return (0);
+}
