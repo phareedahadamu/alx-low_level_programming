9f9743788e7d16ea97aad0698c7edccb794c6a51
Farida <phareedahadamu@gmail.com>
2023-03-13 16:06:29 +0100

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,9 @@
  * Return: 0 on success
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	(void) argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
