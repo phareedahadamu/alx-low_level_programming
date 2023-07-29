89085d57f93a87f583c338ac18f49bf063f950c0
phareedahadamu@gmail.com <phareedahadamu@gmail.com>
2023-07-29 02:18:00 +0100

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,23 @@
 #include "lists.h"
 
 /**
- *
+ * list_len - A function that returns the number of items in a linked list
+ * @h: A pointer to the first node
+ * Return: The number of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	count++;
+	return (count);
+}
