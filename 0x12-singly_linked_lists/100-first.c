937c19c0891f017da3a0ae665769d1cb8ec84ea2
phareedahadamu@gmail.com <phareedahadamu@gmail.com>
2023-08-04 19:36:25 +0100

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * before_main - A function that prints a sentence before the main function
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
