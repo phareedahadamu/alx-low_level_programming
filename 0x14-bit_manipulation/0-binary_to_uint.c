59cc80061ea376d9b32129eb0c32e87b7ecc2eb0
phareedahadamu@gmail.com <phareedahadamu@gmail.com>
2023-08-06 15:55:21 +0100

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,26 +9,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i, rem, base = 1;
-	unsigned long long int result = 0, n = 0;
+	int i;
+	unsigned long result = 0;
 
 	if (b == NULL)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] >= '0' && b[i] <= '1')
-			result = result * 10 + (b[i] - '0');
+			result = result * 2 + (b[i] - '0');
 		else
 			return (0);
-
 	}
-
-	while (result != 0)
-	{
-		rem = result % 10;
-		result = result / 10;
-		n = n + (rem * base);
-		base = base * 2;
-	}
-	return (n);
+	return (result);
 }
