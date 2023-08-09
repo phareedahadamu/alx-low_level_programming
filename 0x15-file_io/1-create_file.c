0ba83a1be5c6828a84f567abb3c105ec8fd85545
phareedahadamu@gmail.com <phareedahadamu@gmail.com>
2023-08-09 22:57:24 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,30 +11,20 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w_size, len = 0;
-	char *buff;
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[len] != '\0')
-	{
-		len++;
-	}
-	buff = malloc(sizeof(char) * (len + 1));
-	if (buff == NULL)
-		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT, 0600);
 	if (fd < 0)
-	{
-		free(buff);
 		return (-1);
-	}
-	w_size = write(fd, buff, len);
-	if (w_size < 0)
+	if (text_content != NULL)
 	{
-		free(buff);
-		return (-1);
+		while (text_content[len] != '\0')
+			len++;
+		w_size = write(fd, text_content, len);
+		if (w_size < 0)
+			return (-1);
 	}
-	free(buff);
 	close(fd);
 	return (1);
 }
