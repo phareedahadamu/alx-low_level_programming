0b2e522b712a013d6196c58837258e571d2a38e4
phareedahadamu@gmail.com <phareedahadamu@gmail.com>
2023-08-09 22:00:23 +0100

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - A function that creates a new file and
+ *               copies a given string to it
+ * @filename: The file
+ * @text_content: The string to be copied to the new file
+ * Return: 1 on success else -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w_size, len = 0;
+	char *buff;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+	buff = malloc(sizeof(char) * (len + 1));
+	if (buff == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR);
+	if (fd < 0)
+	{
+		free(buff);
+		return (-1);
+	}
+	w_size = write(fd, buff, len);
+	if (w_size < 0)
+	{
+		free(buff);
+		return (-1);
+	}
+	free(buff);
+	return (1);
+}
