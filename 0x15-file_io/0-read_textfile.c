9a31579015f3fcd88c32d3dc913454d972e7adf1
phareedahadamu@gmail.com <phareedahadamu@gmail.com>
2023-08-09 19:01:57 +0100

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,9 +14,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char buff[5000];
 
 	if (filename == NULL)
-		return (0); 
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0 || letters == 0)
+	if (fd < 0)
+		return (0);
+	if (letters == 0)
 		return (0);
 	size = read(fd, buff, letters);
 	if (size < 0 || size == 0)
