9473b5219eacb067a7032d17fe72024cf6e12d65
phareedahadamu@gmail.com <phareedahadamu@gmail.com>
2023-08-11 15:20:21 +0100

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,8 +28,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (i <= idx)
 		{
-			if (temp == NULL || temp->next == NULL)
-				return (NULL);
 			temp = temp->next;
 			i++;
 		}
