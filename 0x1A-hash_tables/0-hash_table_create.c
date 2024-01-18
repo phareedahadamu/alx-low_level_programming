da0164d9620dbaaea9424bdd20ba21b06a87ada2
phareedahadamu <phareedahadamu@gmail.com>
2024-01-18 21:41:11 +0100

    Task 0..

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
-	unsigned long int i = 0;
 
+	if (size == 0)
+		return (NULL);
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-		return (NULL);
-	while (i < size)
 	{
-		new_table->array[i] = NULL;
-		i++;
+		free(new_table);
+		return (NULL);
 	}
 	return (new_table);
 }
